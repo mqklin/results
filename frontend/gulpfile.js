b2b570a337d93f6aad8e71a64cbdb51e623a7b5f
mqklin <mqklin@gmail.com>
2015-10-09 00:08:13 +0500

    add gulp-sass

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var babel = require("gulp-babel");
 var clean = require('gulp-clean');
 var watch = require('gulp-watch');
+var sass = require('gulp-sass');
 
 gulp.task("copy", function(){
   return gulp.src(["index.html", "main.js"]).pipe(gulp.dest("build"));
@@ -25,15 +26,22 @@ gulp.task("babel", function(){
     .pipe(gulp.dest("build/src/components"));
 });
 
+gulp.task('sass', function () {
+  gulp.src('src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('build/css'));
+});
+
 gulp.task("clean", function(){
   return gulp.src('build/**/*', {read: false})
-        .pipe(clean());
+    .pipe(clean());
 });
 
 gulp.task("default", ["copy", "vendors", "babel"]);
 
-gulp.task("watch", ["copy", "vendors", "babel"], function(){
+gulp.task("watch", ["copy", "vendors", "babel", "sass"], function(){
 
   gulp.watch('index.html', ["copy"]);
   gulp.watch('src/components/**/*.js', ["babel"]);
+  gulp.watch('src/sass/**/*.scss', ["sass"]);
 });
